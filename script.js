153817a49b584c7de987d6654e096c5f9e114c6c
Dakyuza <nico.garr60@gmail.com>
2025-11-17 09:48:29 +0100

    adding relief on canvas

--- script.js
+++ script.js
@@ -6,4 +6,9 @@ document.getElementById('ToggleTheme').onclick = function() {
 
 const mycanvas = document.getElementById("myCanvas");
 const ctx = mycanvas.getContext("2d");
-
+ctx.shadowColor = 'rgba(0,0,0,0.7)';
+ctx.shadowBlur = 10;
+ctx.shadowOffsetX = 6;
+ctx.shadowOffsetY = 6;
+ctx.fillStyle = '#cee';
+ctx.fillRect(50, 50, 300, 100);
